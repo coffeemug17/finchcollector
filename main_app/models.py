ecd0b3a9798846b45f751205400ab3d64c5ffca2
coffeemug17 <mitulpandey2@gmail.com>
2023-03-01 10:35:58 -0800

    create super user and register model

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Finch(models.Model):
-    species = models.CharField(max_length=100),
-    beakLength = models.IntegerField(),
-    bodyWeight = models.IntegerField(),
-    color = models.CharField(max_length=100),
-    habitat = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    beakLength = models.IntegerField()
+    color = models.CharField(max_length=100)
 
-    
+    def __str__(self):
+        return f'{self.species} ({self.id})'
