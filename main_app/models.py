1103bebfffccb3419c015b4b840e886d4129d369
coffeemug17 <mitulpandey2@gmail.com>
2023-03-01 09:53:33 -0800

    Initialize model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    species = models.CharField(max_length=100),
+    beakLength = models.IntegerField(),
+    bodyWeight = models.IntegerField(),
+    color = models.CharField(max_length=100),
+    habitat = models.CharField(max_length=100)
+
+    
