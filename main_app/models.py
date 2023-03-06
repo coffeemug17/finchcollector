c44dd7eac185271b4ae712b0fe691763ed510374
coffeemug17 <mitulpandey2@gmail.com>
2023-03-05 19:03:51 -0800

    adding toys

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     species = models.CharField(max_length=100)
     beakLength = models.IntegerField()
@@ -9,3 +25,23 @@ class Finch(models.Model):
 
     def __str__(self):
         return f'{self.species} ({self.id})'
+
+
+class Feeding(models.Model):
+    date =models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    # create a foreign key
+    finch = models.ForeignKey(
+        Finch,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+import os
+import uuid
+import boto3
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -17,8 +22,9 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-        'finch': finch
+        'finch': finch, 'feeding_form': feeding_form
     })
 
 class FinchCreate(CreateView):
@@ -34,3 +40,30 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches'
+
+def add_feeding(request, finch_id):
+    # create a modelform
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
